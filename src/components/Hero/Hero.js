2e07cb0f09d613ae500644c66ed02cd41e0f9513
Tomas <t.bojkovski@gmail.com>
2023-03-15 23:51:41 -0400

    adds responsive styling to home page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,11 @@
 import RecommendedMovie from "../RecommendedMovie/RecommendedMovie"
+import "./Hero.scss"
 
 function Hero() {
   return (
-    <section>
-      <h1>Don't know what to watch today? We can help with that</h1>
-      <button>Find Movie</button>
+    <section className="hero">
+      <h1 className="hero__title">Don't know what to watch today? We can help with that</h1>
+      <button className="hero__button">Find Movie</button>
       <RecommendedMovie />
     </section>
   );
